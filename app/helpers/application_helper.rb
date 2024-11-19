5f56e5f72c2024c19e846b81bcc7339e3544d941
velina negovanska <vnegovanska@gmail.com>
2024-11-19 15:55:12 +0100

    Index page avec photos HTML

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def convert_to_euro(num)
+    string_array = num.to_s.chars
+    result = []
+    string_array.reverse.each_slice(3) { |a| result.push(a.join.reverse) }
+    "#{result.reverse.join(' ')}€"
+  end
 end
