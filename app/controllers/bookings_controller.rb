61e4ff573fcf11be677982d9b645a28f94dcd1c2
Roddy Vellayoudom <rodrodvellayoudom@hotmail.com>
2024-11-19 17:09:50 +0100

    ajout de la méthode new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,24 @@
 class BookingsController < ApplicationController
   def index
   @bookings = Booking.all
-  end 
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+    redirect_to bookings_path
+  end
     private
 
   def set_bookings
       @task = Task.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
