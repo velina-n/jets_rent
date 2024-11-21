c153d859537899d48858e356a4aa130b39503e07
Antoine LE FRAPPER <antoine.lefrapper@omexom.com>
2024-11-21 21:53:50 +0100

    changement seed + models +controllers

--- app/models/jet.rb
+++ app/models/jet.rb
@@ -1,7 +1,9 @@
 class Jet < ApplicationRecord
   belongs_to :user
-  has_many :bookings, dependent: :destroy
+  has_many_attached :images
 
-  validates :model, :price, :capacity, presence: true
-  validates :price, numericality: { greater_than: 0 }
+  validates :model, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :capacity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :images, presence: { message: "must be attached" }
 end
