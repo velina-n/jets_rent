8514b3198cf6a4c9e38692e8c51729347000a0bf
Antoine LE FRAPPER <antoine.lefrapper@omexom.com>
2024-11-22 09:54:52 +0100

    boutons

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,20 +1,40 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:accept, :decline]
   def create
     @jet = Jet.find(params[:jet_id]) # Trouve le jet associé
     @booking = @jet.bookings.new(booking_params) # Associe la réservation au jet
     @booking.user = current_user # Associe la réservation à l'utilisateur connecté
 
     if @booking.save
-      redirect_to jet_path(@jet), notice: 'Réservation créée avec succès!'
+      redirect_to dashboard_path, notice: "Réservation créée avec succès !" # Redirection vers le dashboard
     else
-      # Affiche les erreurs de validation pour débogage
-      Rails.logger.debug(@booking.errors.full_messages.join(", "))
-      render 'jets/show', status: :unprocessable_entity
+      flash.now[:alert] = "Une erreur s'est produite. Veuillez vérifier les informations."
+      render "jets/show" # Re-render le formulaire en cas d'erreur
     end
+  end
+
+  # Accepter une réservation
+  def accept
+    @booking.update(status: 'accepted')
+    redirect_to dashboard_path, notice: "Réservation acceptée avec succès."
+  end
+
+  # Refuser une réservation
+  def decline
+    @booking = Booking.find(params[:id])
+    @booking.destroy # Supprime la réservation
+
+    redirect_to dashboard_path, notice: "La réservation a été refusée et supprimée."
   end
 
   private
 
+  # Trouver la réservation
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  # Strong parameters (si nécessaire)
   def booking_params
     params.require(:booking).permit(:airport, :start_date, :end_date, :special_requests)
   end
